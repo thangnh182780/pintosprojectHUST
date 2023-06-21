b5f6e59e1b6e8bf51c1de52c08be61ff35ec5bc9
AnvillKidman <137239396+thangnh182780@users.noreply.github.com>
2023-06-21 11:59:02 +0700

    Add files via upload

---
+++ Accessing user memory/src/userprog/build/filesys/file.d
@@ -0,0 +1,7 @@
+filesys/file.o: ../../filesys/file.c ../../filesys/file.h \
+ ../../filesys/off_t.h ../../lib/stdint.h ../../filesys/inode.h \
+ ../../lib/stdbool.h ../../devices/block.h ../../lib/stddef.h \
+ ../../lib/inttypes.h ../../threads/malloc.h ../../lib/debug.h \
+ ../../lib/kernel/list.h ../../threads/thread.h ../../lib/kernel/list.h \
+ ../../threads/fixed_point.h ../../threads/synch.h \
+ ../../lib/kernel/hash.h ../../lib/kernel/list.h
---
+++ Accessing user memory/src/userprog/build/filesys/filesys.d
@@ -0,0 +1,10 @@
+filesys/filesys.o: ../../filesys/filesys.c ../../filesys/filesys.h \
+ ../../lib/stdbool.h ../../filesys/off_t.h ../../lib/stdint.h \
+ ../../lib/debug.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/stddef.h ../../lib/kernel/stdio.h ../../lib/string.h \
+ ../../filesys/file.h ../../filesys/inode.h ../../devices/block.h \
+ ../../lib/inttypes.h ../../threads/malloc.h ../../lib/kernel/list.h \
+ ../../filesys/free-map.h ../../filesys/directory.h \
+ ../../threads/thread.h ../../lib/kernel/list.h \
+ ../../threads/fixed_point.h ../../threads/synch.h \
+ ../../lib/kernel/hash.h ../../lib/kernel/list.h
